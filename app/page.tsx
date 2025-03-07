4584cebfca6a5472721236daacd4bfd8b7702ab3
wiizzl <wiizz76.contact@gmail.com>
2025-03-07 13:19:06 +0100

    feat: update HomePage layout for mobile-only message and upgrade dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <main>
-      <section className="hidden md:block"></section>
+      <section className="hidden md:flex justify-center items-center h-screen">
+        <p>Ce site est disponible uniquement en version mobile. Changez de périphérique et réessayez</p>
+      </section>
       <section className="md:hidden"></section>
     </main>
   );
